028358b6a5322c1e52ac59ea8a995ebc999eb95e
shannon <shannonrmoranetz@gmail.com>
2019-02-23 18:20:08 -0700

    Add expanded card styling

--- src/containers/CardExpanded/CardExpanded.js
+++ src/containers/CardExpanded/CardExpanded.js
@@ -4,11 +4,18 @@ class CardExpanded extends Component {
   render() {
     return (
       <div className="CardExpanded">
-        <h2 className="cardName">{this.props.name}</h2>
-        <p className="cardAmiiboSeries"> amiibo series: {this.props.amiiboSeries}</p>
-        <p className="cardGameSeries">game series: {this.props.gameSeries}</p>
-        <p className="cardDate">release date: {this.props.release.na}</p>
+        <div className="innerCard">
+          <div className="cardImageContainer">
+            <img className="previewImageExpanded" src={this.props.image} alt={`${this.props.name} amiibo`}/>
+          </div>
+          <div className="cardInfo">
+            <h2 className="cardName">{this.props.name}</h2>
+            <p className="cardAmiiboSeries"> amiibo series: {this.props.amiiboSeries}</p>
+            <p className="cardGameSeries">game series: {this.props.gameSeries}</p>
+            <p className="cardDate">release date: {this.props.release.na}</p>
+          </div>
       </div>
+    </div>
     );
   }
 }
