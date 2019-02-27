d37f6802ac9c66d6cce58c042548a50b70eed912
shannon <shannonrmoranetz@gmail.com>
2019-02-27 03:09:30 -0700

    Add active class to navLink

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ## Project Description:
   Miibah is an application that gives users a fun and friendly interface to keep track of their Amiibo collection with. When using Miibah, a user can either save Amiibos to a wishlist or a list of collected Amiibos if they already own it. 
 
+## Current Version:
+[Current Version](https://i.imgur.com/ZB48UuY.gifv)
+
 ## Getting Started:
 Follow the instructions below to install the app:
 
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const Menu = () => {
   return (
     <div className="Menu">
-      <NavLink to='/' className="nav-button home-button">home</NavLink>
-      <NavLink to='/wishlist' className="nav-button wishlist-button">wishlist</NavLink>
-      <NavLink to='/collected' className="nav-button collection-button">collected</NavLink>
+      <NavLink exact to='/' className="nav-button home-button" activeStyle={{ color: '#a6a6eb' }}>home</NavLink>
+      <NavLink to='/wishlist' className="nav-button wishlist-button" activeStyle={{ color: '#a6a6eb' }}>wishlist</NavLink>
+      <NavLink to='/collected' className="nav-button collection-button" activeStyle={{ color: '#a6a6eb' }}>collected</NavLink>
     </div>
   );
 }
